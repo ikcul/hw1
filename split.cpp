9ad77f2921de12cabacb801997ce5014e6625c8a
Daniel Sim <simdanie@usc.edu>
2025-09-09 18:40:19 +0000

    beginning work on split.cpp

--- split.cpp
+++ split.cpp
@@ -18,6 +18,29 @@ void split(Node*& in, Node*& odds, Node*& evens)
 {
   /* Add code here */
 // WRITE YOUR CODE HERE
+  //already sorted
+
+  //check if in is nullptr
+  //check if odds is nullptr
+  //check if evens is nullptr
+  //else add first value into either odds and even
+  //unlink the node and attribute it to its respective list
+
+
+  Node* temp = new Node;
+  temp->val = in->val;
+  temp->next = in->next;
+
+  if (in == nullptr){
+    return;
+  }else if(temp->val % 2 == 1 && odds == nullptr){
+    odds->val = temp->val;
+    odds->next = nullptr;
+    //unlink the node from the original list
+    temp->next = 
+  }
+
+
 }
 
 /* If you needed a helper function, write it here */
