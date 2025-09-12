32a7cd26eb5d599873ccdc3aef3b1867f3c870d5
Daniel <simdaniel06@gmail.com>
2025-09-12 15:57:12 -0700

    ulliststr.cpp test cases

--- test_ulliststr.cpp
+++ test_ulliststr.cpp
@@ -9,5 +9,53 @@
 
 int main(int argc, char* argv[])
 {
+    ULListStr list;
 
+    std::cout << "Push back and push front" << std::endl;
+    list.push_back("1");
+    list.push_back("2");
+    list.push_back("3");
+    list.push_front("4");
+    list.push_front("5");
+    list.push_front("6");
+
+    std::cout << "Expected: 6 5 4 1 2 3" << std::endl;
+    std::cout << "Result: ";
+    for (size_t i = 0; i < list.size(); i++) {
+        std::cout << list.get(i) << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "Pop front and pop back" << std::endl;
+    list.pop_front(); 
+    list.pop_back();
+
+    std::cout << "Expected: 5 4 1 2" << std::endl;
+    std::cout << "Result: ";
+    for (size_t i = 0; i < list.size(); i++) {
+        std::cout << list.get(i) << " ";
+    }
+    std::cout << std::endl;
+
+
+    std::cout << "Set and Get" << std::endl;
+    list.set(1, "123");
+    std::cout << "Expected: 5 123 1 2" << std::endl;
+    std::cout << "Result: ";
+    for (size_t i = 0; i < list.size(); i++) {
+        std::cout << list.get(i) << " ";
+    }
+    std::cout << std::endl;
+
+
+    std::cout << "Empty list" << std::endl;
+    ULListStr empty;
+    if (empty.empty()){
+         std::cout << "true" << std::endl;
+    }else{
+        std::cout << "false" << std::endl;
+    }
+    std::cout << "Size: " << empty.size() << std::endl;
+
+    return 0;
 }
